ef4421c1e482689428c030f47f430a78356e57e6
Ryan <543764210@qq.com>
2016-11-12 13:18:06 +0800

    first_commit

---
+++ Assets/MyUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyUI : MonoBehaviour {
+	public Material body;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	public void ButtondownRed(){
+		body.color = Color.red;
+	}
+	public void Buttondownblue(){
+		body.color = Color.blue;
+	}
+}
